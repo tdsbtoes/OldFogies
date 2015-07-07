c68c06fea643695134f45d1df27bb4c6e27ecbba
tdsbtoes <chris.metropolis@tdsb.on.ca>
2015-07-06 23:54:20 -0400

    1.5
    added battery level depletion

--- src/ks-clock-face.c
+++ src/ks-clock-face.c
@@ -9,6 +9,16 @@
 #define ANIMATION_DURATION 500
 #define ANIMATION_DELAY    600
 
+// Languages
+#define LANG_DUTCH 0
+#define LANG_ENGLISH 1
+#define LANG_FRENCH 2
+#define LANG_GERMAN 3
+#define LANG_SPANISH 4
+#define LANG_PORTUGUESE 5
+#define LANG_SWEDISH 6
+#define LANG_MAX 7
+
 typedef struct {
   int hours;
   int minutes;
@@ -30,6 +40,13 @@ static TextLayer *s_day_date;
 static char s_day_buffer[12];
 
 static GPath *s_min_path;
+
+static uint8_t battery_colours[] = {
+	GColorRedARGB8,
+	GColorYellowARGB8,
+	GColorShockingPinkARGB8,
+	GColorLightGrayARGB8
+};
 
 static uint8_t face_colours[] = {
 	GColorIslamicGreenARGB8,
@@ -81,14 +98,92 @@ static uint8_t face_colours[] = {
 	GColorKellyGreenARGB8,
 		GColorMintGreenARGB8,
 	GColorBulgarianRoseARGB8,
-		GColorPurpureusARGB8
+		GColorPurpureusARGB8,
+	GColorBlackARGB8,
+	GColorWhiteARGB8
+};
+
+// first bg colour index
+static int s_colour_a = -1;
+
+// second bg colour index
+static int s_colour_b;
+
+// store index for the colour name from the face_colours array
+static uint8_t main_colour;
+static uint8_t minute_colour;
+static int b_angle = 45; // for battery arc
+static int battery_level;
+static bool battery_charging;
+static BatteryChargeState charge_state;
+
+// user choices - set defaults here
+static int user_lang = 1; 							//default to english
+static bool user_show_battery = true;				//default to ON
+static bool user_show_date = true;					//default to ON
+static int user_circle_minute_stroke_colour = 50; 	//default black (the 50 may change as the list changes)
+static int user_circle_minute_outline_colour = 51; 	//default white
+static int user_battery_colour = 0; 				//default red = 0
+static int user_circle_fill = 51; 					//default white
+static int user_hour_text_colour = 50; 				//default black
+static int user_date_fill = 51; 					//default white
+static int user_date_text_colour = 50; 				//default black
+static int user_date_stroke_colour = 50;			//default black
+static int user_date_outline_colour = 51;			//default white
+
+
+// common coordinates
+static GPoint screen_centre = { 72, 84 };
+static GPoint screen_top_centre = { 72, -1 }; // find it works better to be slightly "outside" the screen for the corners
+static GPoint screen_top_right = { 145, -1 };
+static GPoint screen_bottom_right = { 145, 169 };
+static GPoint screen_bottom_left = { -1, 169 };
+static GPoint screen_top_left = { -1, -1 };
+
+
+
+const char weekDay[LANG_MAX][7][6] = {
+	{ "zon", "maa", "din", "woe", "don", "vri", "zat" },	// Dutch
+	{ "sun", "mon", "tue", "wed", "thu", "fri", "sat" },	// English
+	{ "dim", "lun", "mar", "mer", "jeu", "ven", "sam" },	// French
+	{ "son", "mon", "die", "mit", "don", "fre", "sam" },	// German
+	{ "dom", "lun", "mar", "mie", "jue", "vie", "sab" },	// Spanish
+	{ "dom", "seg", "ter", "qua", "qui", "sex", "sab" },	// Portuguese
+	{ "sön", "mån", "Tis", "ons", "tor", "fre", "lör" }		// Swedish
 };
 
 
-static int s_colour_a = -1;
-static int s_colour_b;
-static uint8_t main_colour;
-static uint8_t minute_colour;
+
+
+
+
+void batteryLineArc(GContext *ctx, float start_angle, float end_angle, GPoint centre, int radius, int thickness){
+	
+	float minus_bit = (radius - thickness/2 + 0.5); //add 0.5 for the int casting, later
+	float add_bit = (radius + thickness/2 + 0.5); 
+	
+	graphics_context_set_stroke_width(ctx, 1);
+	
+	if (start_angle == 0) {
+		start_angle = 2.5;	// for some reason, 0 seems to start too far to the left....at least for my purposes
+	}
+	
+	for (float i = start_angle; i <= end_angle; i+=0.5) {
+		
+		GPoint inside_point = (GPoint) {
+			.x = (int16_t)(sin_lookup(i * TRIG_MAX_ANGLE / 360) * minus_bit / TRIG_MAX_RATIO) + centre.x,
+			.y = (int16_t)(-cos_lookup(i * TRIG_MAX_ANGLE / 360) * minus_bit / TRIG_MAX_RATIO) + centre.y,
+		};
+
+		GPoint outside_point = (GPoint) {
+			.x = (int16_t)(sin_lookup(i * TRIG_MAX_ANGLE / 360) * add_bit / TRIG_MAX_RATIO) + centre.x,
+			.y = (int16_t)(-cos_lookup(i * TRIG_MAX_ANGLE / 360) * add_bit / TRIG_MAX_RATIO) + centre.y,
+		};
+		
+		graphics_draw_line(ctx, inside_point, outside_point);
+	}
+}
+
 
 /*************************** AnimationImplementation **************************/
 
@@ -118,6 +213,10 @@ static void animate(int duration, int delay, AnimationImplementation *implementa
 
 /************************************ UI **************************************/
 
+
+
+
+
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
   // Store time
   s_last_time.hours = tick_time->tm_hour;
@@ -146,6 +245,12 @@ static void tick_handler(struct tm *tick_time, TimeUnits changed) {
 		//APP_LOG(APP_LOG_LEVEL_DEBUG, "%d, %d", s_colour_a, s_colour_b);
 	}
 	
+	if (user_show_battery) {
+		charge_state = battery_state_service_peek();
+		battery_level = charge_state.charge_percent;
+		battery_charging = charge_state.is_charging;
+	}
+	
   // Redraw
   if(s_canvas_layer) {
     layer_mark_dirty(s_canvas_layer);
@@ -156,13 +261,14 @@ static void tick_handler(struct tm *tick_time, TimeUnits changed) {
 
 static void update_proc(Layer *layer, GContext *ctx) {
 	// Don't use current time while animating
-  Time mode_time = (s_animating) ? s_anim_time : s_last_time;
+  	Time mode_time = (s_animating) ? s_anim_time : s_last_time;
 	
 	// Get a tm structure
 	time_t temp = time(NULL); 
 	struct tm *tick_time = localtime(&temp);
+	
 
-  // hour text
+  	// hour text
 	if (!s_animating) {
 		// Create a long-lived buffer
 		static char buffer[] = "00";
@@ -177,7 +283,7 @@ static void update_proc(Layer *layer, GContext *ctx) {
 		}
 
 		if (!clock_is_24h_style() && mode_time.hours > 0 && mode_time.hours < 10) {
-			// remove the leading "0""
+			// remove the leading "0"
 			snprintf(buffer, 2, "%d", mode_time.hours);
 		}
 		else if (!clock_is_24h_style() && mode_time.hours == 0) {
@@ -189,18 +295,15 @@ static void update_proc(Layer *layer, GContext *ctx) {
 		text_layer_set_text(s_hour_digit, buffer);
 	}
 	
-  // Color background?
-  if(COLORS) {
-    //graphics_context_set_fill_color(ctx, GColorFromRGB(s_color_channels[0], s_color_channels[1], s_color_channels[2]));
+	// Color background for first colour
 	main_colour = face_colours[s_colour_a];
 	graphics_context_set_fill_color(ctx, (GColor)main_colour);
-    graphics_fill_rect(ctx, GRect(0, 0, 144, 168), 0, GCornerNone);
-  }
-
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+	graphics_fill_rect(ctx, GRect(0, 0, 144, 168), 0, GCornerNone);
   
+	// minute-hand and circle colour
+  	graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  graphics_context_set_antialiased(ctx, ANTIALIASING);
+  	graphics_context_set_antialiased(ctx, ANTIALIASING);
 	
 	
 	// Plot hands
@@ -210,31 +313,61 @@ static void update_proc(Layer *layer, GContext *ctx) {
 	};
 
 	
-	// Fill in minute color (aka s_colour_b)
+	// Fill in minute color (aka s_colour_b) -- basic is a triangle in the first eighth of the clock (< 7 minutes)
 	GPathInfo s_min_points =  {
-		  .num_points = 3,
-		  .points = (GPoint []) {{72, -1}, {72, 84}, {minute_hand.x, minute_hand.y}}
-		};
+		.num_points = 3,
+		.points = (GPoint []) {
+			screen_top_centre, 
+			screen_centre, 
+			{minute_hand.x, minute_hand.y}
+		}
+	};
 	
 	if (mode_time.minutes > 7 && mode_time.minutes < 23) {
 		// add in the top-right corner
 		s_min_points.num_points = 4;
-		s_min_points.points = (GPoint []) {{72, -1}, {72, 84}, {minute_hand.x, minute_hand.y}, {145, -1}};
+		s_min_points.points = (GPoint []) {
+			screen_top_centre, 
+			screen_centre, 
+			{minute_hand.x, minute_hand.y}, 
+			screen_top_right
+		};
 	}
 	else if (mode_time.minutes > 22 && mode_time.minutes < 38) {
 		// add in bottom-right and top-right
 		s_min_points.num_points = 5,
-		s_min_points.points = (GPoint []) {{72, -1}, {72, 84}, {minute_hand.x, minute_hand.y}, {145, 169}, {145, -1}};
+		s_min_points.points = (GPoint []) {
+			screen_top_centre, 
+			screen_centre, 
+			{minute_hand.x, minute_hand.y},  
+			screen_bottom_right, 
+			screen_top_right
+		};
 	}
 	else if (mode_time.minutes > 37 && mode_time.minutes < 52) {
 		// bottom-left, bottom-right & top-right
-		 s_min_points.num_points = 6,
-		s_min_points.points = (GPoint []) {{72, -1}, {72, 84}, {minute_hand.x, minute_hand.y}, {-1, 169}, {145, 169}, {145, -1}};
+		s_min_points.num_points = 6,
+		s_min_points.points = (GPoint []) {
+			screen_top_centre, 
+			screen_centre, 
+			{minute_hand.x, minute_hand.y}, 
+			screen_bottom_left, 
+			screen_bottom_right, 
+			screen_top_right
+		};
 	}
 	else if (mode_time.minutes > 51 && mode_time.minutes <= 59) {
 		//*/ going negative seems to help along the left edge - draw to all four corners
 		s_min_points.num_points = 7,
-		s_min_points.points = (GPoint []) {{72, -1}, {72, 84}, {minute_hand.x, minute_hand.y}, {-1, -1}, {-1, 169}, {145, 169}, {145, -1}};
+		s_min_points.points = (GPoint []) {
+			screen_top_centre, 
+			screen_centre, 
+			{minute_hand.x, minute_hand.y}, 
+			screen_top_left, 
+			screen_bottom_left, 
+			screen_bottom_right, 
+			screen_top_right
+		};
 	}
 	
 	// set up
@@ -264,24 +397,38 @@ static void update_proc(Layer *layer, GContext *ctx) {
 	
 	
 	
-	// White clockface
+	// fill clockface
 	graphics_context_set_fill_color(ctx, GColorWhite);
 	graphics_fill_circle(ctx, s_center, s_radius);
 	
   	
-	// draw black circle outline
+	// draw black circle 
 	graphics_context_set_stroke_color(ctx, GColorBlack);
 	graphics_context_set_stroke_width(ctx, minute_stroke);
   	graphics_draw_circle(ctx, s_center, s_radius);
 
 	
-	// date box
+	// date box fill
+	graphics_fill_rect(ctx, GRect(40, 131, 64, 24), 10, GCornersAll);
+	
+	// date box stroke
 	graphics_context_set_stroke_color(ctx, GColorBlack);
 	graphics_context_set_stroke_width(ctx, 1);
-	graphics_fill_rect(ctx, GRect(40, 131, 64, 24), 10, GCornersAll);
 	graphics_draw_round_rect(ctx, GRect(40, 131, 64, 24), 10);
+	
+	// date box content -- weekDay[user_lang][tick_time->tm_wday]
 	strftime(s_day_buffer, sizeof("ddd dd"), "%a %d", tick_time);
 	text_layer_set_text(s_day_date, s_day_buffer);
+	
+	// if showing battery level, draw arc
+	if (user_show_battery) {
+		graphics_context_set_stroke_color(ctx, (GColor)battery_colours[user_battery_colour]);
+		
+		if (!battery_charging) {
+			b_angle = (int)((360 * ((100 - battery_level)) / 100) + 0.5);
+			batteryLineArc(ctx, 0, b_angle, s_center, s_radius, minute_stroke);
+		}
+	}
 	
 	s_startup = false;
 	
@@ -300,8 +447,10 @@ static void window_load(Window *window) {
   layer_set_update_proc(s_canvas_layer, update_proc);
   layer_add_child(window_layer, s_canvas_layer);
 	
+	//battery_state_service_subscribe(battery_handler);
+	
 	// Create time TextLayer
-	s_hour_digit = text_layer_create(GRect(41, 57, 62, 50));
+	s_hour_digit = text_layer_create(GRect(41, 58, 62, 50));
 	text_layer_set_background_color(s_hour_digit, GColorClear);
 	text_layer_set_text_color(s_hour_digit, GColorBlack);
 	
@@ -327,6 +476,8 @@ static void window_unload(Window *window) {
   	text_layer_destroy(s_hour_digit);
 	text_layer_destroy(s_day_date);
 	layer_destroy(s_canvas_layer);
+	battery_state_service_unsubscribe();
+	tick_timer_service_unsubscribe();
 }
 
 /*********************************** App **************************************/
